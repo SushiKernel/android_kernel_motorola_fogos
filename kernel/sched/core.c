05f1a92aaca6d5f4248eda6e0bd67af79bc2df3d
Yaroslav Furman <yaro330@gmail.com>
2019-05-28 19:34:06 +0300

    {irq,sched}: silence 'no longer affine to cpu' logspam
    Co-authored-by: sbwml <984419930@qq.com>
    Signed-off-by: sbwml <984419930@qq.com>
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -3208,7 +3208,7 @@ out:
 		 * leave kernel.
 		 */
 		if (p->mm && printk_ratelimit()) {
-			printk_deferred("process %d (%s) no longer affine to cpu%d\n",
+			pr_debug("process %d (%s) no longer affine to cpu%d\n",
 					task_pid_nr(p), p->comm, cpu);
 		}
 	}
