3a45f1d226d827a647ffa21e8943ffe29a1a4912
Frederic Weisbecker <frederic@kernel.org>
2021-04-09 00:38:59 +0200

    srcu: Initialize SRCU after timers
    Once srcu_init() is called, the SRCU core will make use of delayed
    workqueues, which rely on timers.  However init_timers() is called
    several steps after rcu_init().  This means that a call_srcu() after
    rcu_init() but before init_timers() would find itself within a dangerously
    uninitialized timer core.
    
    This commit therefore creates a separate call to srcu_init() after
    init_timer() completes, which ensures that we stay in early SRCU mode
    until timers are safe(r).
    
    Signed-off-by: Frederic Weisbecker <frederic@kernel.org>
    Cc: Uladzislau Rezki <urezki@gmail.com>
    Cc: Boqun Feng <boqun.feng@gmail.com>
    Cc: Lai Jiangshan <jiangshanlai@gmail.com>
    Cc: Neeraj Upadhyay <neeraju@codeaurora.org>
    Cc: Josh Triplett <josh@joshtriplett.org>
    Cc: Joel Fernandes <joel@joelfernandes.org>
    Signed-off-by: Paul E. McKenney <paulmck@kernel.org>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- kernel/rcu/tree.c
+++ kernel/rcu/tree.c
@@ -4806,7 +4806,6 @@ void __init rcu_init(void)
 	rcu_gp_wq = alloc_workqueue("rcu_gp", WQ_MEM_RECLAIM | WQ_UNBOUND, 0);
 	WARN_ON(!rcu_gp_wq);
 	rcu_alloc_par_gp_wq();
-	srcu_init();
 
 	/* Fill in default value for rcutree.qovld boot parameter. */
 	/* -After- the rcu_node ->lock fields are initialized! */
