9c06941b03df6ef72f6f2909cfd4f714dd93c571
Christoph Hellwig <hch@lst.de>
2021-10-19 15:44:04 +0200

    dm integrity: use bvec_kmap_local in __journal_read_write
    Using local kmaps slightly reduces the chances to stray writes, and
    the bvec interface cleans up the code a little bit.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Mike Snitzer <snitzer@redhat.com>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- drivers/md/dm-integrity.c
+++ drivers/md/dm-integrity.c
@@ -1763,7 +1763,7 @@ static bool __journal_read_write(struct dm_integrity_io *dio, struct bio *bio,
 		n_sectors -= bv.bv_len >> SECTOR_SHIFT;
 		bio_advance_iter(bio, &bio->bi_iter, bv.bv_len);
 retry_kmap:
-		mem = kmap_atomic(bv.bv_page);
+		mem = bvec_kmap_local(&bv);
 		if (likely(dio->write))
 			flush_dcache_page(bv.bv_page);
 
@@ -1777,7 +1777,7 @@ retry_kmap:
 
 				if (unlikely(journal_entry_is_inprogress(je))) {
 					flush_dcache_page(bv.bv_page);
-					kunmap_atomic(mem);
+					kunmap_local(mem);
 
 					__io_wait_event(ic->copy_to_journal_wait, !journal_entry_is_inprogress(je));
 					goto retry_kmap;
@@ -1868,7 +1868,7 @@ retry_kmap:
 
 		if (unlikely(!dio->write))
 			flush_dcache_page(bv.bv_page);
-		kunmap_atomic(mem);
+		kunmap_local(mem);
 	} while (n_sectors);
 
 	if (likely(dio->write)) {
