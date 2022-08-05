15124c4942b5c1def2727cedcfa2c85a831c4b1b
Paul E. McKenney <paulmck@kernel.org>
2022-08-04 17:43:53 -0700

    rcu: Make synchronize_rcu_expedited() fast path update .expedited_sequence
    This commit causes the early boot single-CPU synchronize_rcu_expedited()
    fastpath to update the rcu_state structure's ->expedited_sequence
    counter.  This will allow the full-state polled grace-period APIs to
    detect all expedited grace periods without the need to track the special
    combined polling-only counter, which is another step towards removing
    the ->rgos_polled field from the rcu_gp_oldstate, thereby reducing its
    size by one third.
    
    Signed-off-by: Paul E. McKenney <paulmck@kernel.org>

--- kernel/rcu/tree_exp.h
+++ kernel/rcu/tree_exp.h
@@ -912,6 +912,7 @@ static int rcu_print_task_exp_stall(struct rcu_node *rnp)
 void synchronize_rcu_expedited(void)
 {
 	bool no_wq;
+	unsigned long flags;
 	struct rcu_exp_work rew;
 	struct rcu_node *rnp;
 	unsigned long s;
@@ -930,6 +931,11 @@ void synchronize_rcu_expedited(void)
 		// them, which allows reuse of ->gp_seq_polled_exp_snap.
 		rcu_poll_gp_seq_start_unlocked(&rcu_state.gp_seq_polled_exp_snap);
 		rcu_poll_gp_seq_end_unlocked(&rcu_state.gp_seq_polled_exp_snap);
+
+		local_irq_save(flags);
+		WARN_ON_ONCE(num_online_cpus() > 1);
+		rcu_state.expedited_sequence += (1 << RCU_SEQ_CTR_SHIFT);
+		local_irq_restore(flags);
 		return;  // Context allows vacuous grace periods.
 	}
 
