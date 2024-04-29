207ce4549c7804db369eb70548a316c67484a29b
Valentin Schneider <vschneid@redhat.com>
2024-04-29 16:43:36 +0200

    rcu: Rename rcu_momentary_dyntick_idle() into rcu_momentary_eqs()
    [ Upstream commit 32a9f26e5e266f0116cc8536e5e4544523ddd334 ]
    
    The context_tracking.state RCU_DYNTICKS subvariable has been renamed to
    RCU_WATCHING, replace "dyntick_idle" into "eqs" to drop the dyntick
    reference.
    
    Signed-off-by: Valentin Schneider <vschneid@redhat.com>
    Reviewed-by: Frederic Weisbecker <frederic@kernel.org>
    Signed-off-by: Neeraj Upadhyay <neeraj.upadhyay@kernel.org>
    Stable-dep-of: 27d73e81195b ("rcu: Mark accesses to ->rcu_urgent_qs and ->rcu_need_heavy_qs")
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- kernel/rcu/tree.c
+++ kernel/rcu/tree.c
@@ -427,7 +427,7 @@ bool rcu_dynticks_zero_in_eqs(int cpu, int *vp)
  *
  * The caller must have disabled interrupts and must not be idle.
  */
-notrace void rcu_momentary_dyntick_idle(void)
+notrace void rcu_momentary_eqs(void)
 {
 	int seq;
 
@@ -437,7 +437,7 @@ notrace void rcu_momentary_dyntick_idle(void)
 	WARN_ON_ONCE(!(seq & 0x1));
 	rcu_preempt_deferred_qs(current);
 }
-EXPORT_SYMBOL_GPL(rcu_momentary_dyntick_idle);
+EXPORT_SYMBOL_GPL(rcu_momentary_eqs);
 
 /**
  * rcu_is_cpu_rrupt_from_idle - see if 'interrupted' from idle
