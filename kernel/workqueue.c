7318277b5caa01558ad5046d75767d9c974152b6
Lai Jiangshan <jiangshan.ljs@antgroup.com>
2024-06-21 15:32:25 +0800

    workqueue: Remove useless pool->dying_workers
    A dying worker is first moved from pool->workers to pool->dying_workers
    in set_worker_dying() and removed from pool->dying_workers in
    detach_dying_workers().  The whole procedure is in the some lock context
    of wq_pool_attach_mutex.
    
    So pool->dying_workers is useless, just remove it and keep the dying
    worker in pool->workers after set_worker_dying() and remove it in
    detach_dying_workers() with wq_pool_attach_mutex held.
    
    Cc: Valentin Schneider <vschneid@redhat.com>
    Signed-off-by: Lai Jiangshan <jiangshan.ljs@antgroup.com>
    Signed-off-by: Tejun Heo <tj@kernel.org>

--- kernel/workqueue.c
+++ kernel/workqueue.c
@@ -217,7 +217,6 @@ struct worker_pool {
 
 	struct worker		*manager;	/* L: purely informational */
 	struct list_head	workers;	/* A: attached workers */
-	struct list_head        dying_workers;  /* A: workers about to die */
 
 	struct ida		worker_ida;	/* worker IDs for task name */
 
@@ -2890,7 +2889,6 @@ static void set_worker_dying(struct worker *worker, struct list_head *list)
 	worker->flags |= WORKER_DIE;
 
 	list_move(&worker->entry, list);
-	list_move(&worker->node, &pool->dying_workers);
 
 	/* get an extra task struct reference for later kthread_stop_put() */
 	get_task_struct(worker->task);
@@ -4745,7 +4743,6 @@ static int init_worker_pool(struct worker_pool *pool)
 	timer_setup(&pool->mayday_timer, pool_mayday_timeout, 0);
 
 	INIT_LIST_HEAD(&pool->workers);
-	INIT_LIST_HEAD(&pool->dying_workers);
 
 	ida_init(&pool->worker_ida);
 	INIT_HLIST_NODE(&pool->hash_node);
