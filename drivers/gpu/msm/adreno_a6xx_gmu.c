5c0e28aaf63b65f41b4578651d11b5a7ee3e9b65
Kamal Agrawal <quic_kamaagra@quicinc.com>
2025-10-18 17:06:57 +0000

    msm: kgsl: Avoid inline submission if slumber is in progress
    Consider a scenario where usermode is submitting commands while slumber
    is in progress. During slumber sequence, device mutex is held by kgsl and
    it polls for CX gdsc to go down. It is possible that SMMU can keep a vote
    on CX due to ongoing iommu operations and it may take longer for CX gdsc
    to go down.
    
    In another thread, say usermode is submitting commands, currently these
    commands are submitted inline to the GPU for gmu based targets. However,
    while submitting commands to GPU, device mutex is needed. Usermode thread
    will be blocked on device mutex as it is held by slumber thread. This can
    result in UI freeze or janks.
    
    To resolve this, avoid inline submission whenever slumber is in progress.
    Add relevant code to properly update the slumber entry and exit flag.
    
    (cherry picked from commit 6812a9ac4197c87f6c25daef83a018ac09446f43)
    Signed-off-by: TogoFire <togofire@mailfence.com>
    Change-Id: I09b518d4b0fe78bbc855ddcdc320773f7c134b78

--- drivers/gpu/msm/adreno_a6xx_gmu.c
+++ drivers/gpu/msm/adreno_a6xx_gmu.c
@@ -2197,7 +2197,7 @@ static int a6xx_gmu_first_boot(struct adreno_device *adreno_dev)
 
 	device->gmu_fault = false;
 
-	trace_kgsl_pwr_set_state(device, KGSL_STATE_AWARE);
+	kgsl_pwrctrl_set_state(device, KGSL_STATE_AWARE);
 
 	return 0;
 
@@ -2279,7 +2279,7 @@ static int a6xx_gmu_boot(struct adreno_device *adreno_dev)
 
 	device->gmu_fault = false;
 
-	trace_kgsl_pwr_set_state(device, KGSL_STATE_AWARE);
+	kgsl_pwrctrl_set_state(device, KGSL_STATE_AWARE);
 
 	return 0;
 
@@ -2926,9 +2926,8 @@ static int a6xx_boot(struct adreno_device *adreno_dev)
 	set_bit(GMU_PRIV_GPU_STARTED, &gmu->flags);
 
 	device->pwrctrl.last_stat_updated = ktime_get();
-	device->state = KGSL_STATE_ACTIVE;
 
-	trace_kgsl_pwr_set_state(device, KGSL_STATE_ACTIVE);
+	kgsl_pwrctrl_set_state(device, KGSL_STATE_ACTIVE);
 
 	return ret;
 }
@@ -3010,9 +3009,8 @@ static int a6xx_first_boot(struct adreno_device *adreno_dev)
 		adreno_dev->bcl_enabled = true;
 
 	device->pwrctrl.last_stat_updated = ktime_get();
-	device->state = KGSL_STATE_ACTIVE;
 
-	trace_kgsl_pwr_set_state(device, KGSL_STATE_ACTIVE);
+	kgsl_pwrctrl_set_state(device, KGSL_STATE_ACTIVE);
 
 	place_marker("M - DRIVER ADRENO Ready");
 
@@ -3104,7 +3102,7 @@ no_gx_power:
 
 	kgsl_pwrctrl_clear_l3_vote(device);
 
-	trace_kgsl_pwr_set_state(device, KGSL_STATE_SLUMBER);
+	kgsl_pwrctrl_set_state(device, KGSL_STATE_SLUMBER);
 
 	return ret;
 }
@@ -3122,8 +3120,21 @@ static void gmu_idle_check(struct work_struct *work)
 		goto done;
 
 	if (!atomic_read(&device->active_cnt)) {
-		if (test_bit(GMU_PRIV_GPU_STARTED, &gmu->flags))
+		if (test_bit(GMU_PRIV_GPU_STARTED, &gmu->flags)) {
+			spin_lock(&device->submit_lock);
+
+			if (device->submit_now) {
+				spin_unlock(&device->submit_lock);
+				kgsl_pwrscale_update(device);
+				kgsl_start_idle_timer(device);
+				goto done;
+			}
+
+			device->slumber = true;
+			spin_unlock(&device->submit_lock);
+
 			a6xx_power_off(adreno_dev);
+		}
 	} else {
 		kgsl_pwrscale_update(device);
 		kgsl_start_idle_timer(device);
@@ -3225,7 +3236,7 @@ static int a6xx_gmu_pm_suspend(struct adreno_device *adreno_dev)
 
 	adreno_dispatcher_halt(device);
 
-	trace_kgsl_pwr_set_state(device, KGSL_STATE_SUSPEND);
+	kgsl_pwrctrl_set_state(device, KGSL_STATE_SUSPEND);
 
 	return 0;
 err:
@@ -3281,9 +3292,8 @@ static void a6xx_gmu_touch_wakeup(struct adreno_device *adreno_dev)
 	set_bit(GMU_PRIV_GPU_STARTED, &gmu->flags);
 
 	device->pwrctrl.last_stat_updated = ktime_get();
-	device->state = KGSL_STATE_ACTIVE;
 
-	trace_kgsl_pwr_set_state(device, KGSL_STATE_ACTIVE);
+	kgsl_pwrctrl_set_state(device, KGSL_STATE_ACTIVE);
 }
 
 const struct adreno_power_ops a6xx_gmu_power_ops = {
