7e9ca9994ab71dbaa4b41169f6f0f4adb16f51ec
Zhen Ni <nizhen@uniontech.com>
2026-02-16 14:04:21 +0000

    BACKPORT: sched: Move rt_period/runtime sysctls to rt.c
    move rt_period/runtime sysctls to rt.c and use the new
    register_sysctl_init() to register the sysctl interface.
    
    Signed-off-by: Zhen Ni <nizhen@uniontech.com>
    Signed-off-by: Luis Chamberlain <mcgrof@kernel.org>

--- include/linux/sched/sysctl.h
+++ include/linux/sched/sysctl.h
@@ -46,15 +46,6 @@ int sched_proc_update_handler(struct ctl_table *table, int write,
 		void *buffer, size_t *length, loff_t *ppos);
 #endif
 
-/*
- *  control realtime throttling:
- *
- *  /proc/sys/kernel/sched_rt_period_us
- *  /proc/sys/kernel/sched_rt_runtime_us
- */
-extern unsigned int sysctl_sched_rt_period;
-extern int sysctl_sched_rt_runtime;
-
 extern unsigned int sysctl_sched_dl_period_max;
 extern unsigned int sysctl_sched_dl_period_min;
 
@@ -72,8 +63,6 @@ extern int sysctl_sched_rr_timeslice;
 extern int sched_rr_timeslice;
 
 int sched_rr_handler(struct ctl_table *table, int write, void *buffer,
-		size_t *lenp, loff_t *ppos);
-int sched_rt_handler(struct ctl_table *table, int write, void *buffer,
 		size_t *lenp, loff_t *ppos);
 int sysctl_sched_uclamp_handler(struct ctl_table *table, int write,
 		void *buffer, size_t *lenp, loff_t *ppos);
