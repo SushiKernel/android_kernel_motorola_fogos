306c006bfb153dd9367f717520298ea014688fb5
Thomas Gleixner <tglx@linutronix.de>
2026-02-20 18:52:05 +0000

    sched: Move kprobes cleanup out of finish_task_switch()
    Doing cleanups in the tail of schedule() is a latency punishment for the
    incoming task. The point of invoking kprobes_task_flush() for a dead task
    is that the instances are returned and cannot leak when __schedule() is
    kprobed.
    
    Move it into the delayed cleanup.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Link: https://lkml.kernel.org/r/20210928122411.537994026@linutronix.de
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- kernel/exit.c
+++ kernel/exit.c
@@ -65,6 +65,7 @@
 #include <linux/compat.h>
 #include <linux/sysfs.h>
 #include <linux/usermode_driver.h>
+#include <linux/kprobes.h>
 
 #include <linux/uaccess.h>
 #include <asm/unistd.h>
@@ -225,6 +226,7 @@ static void delayed_put_task_struct(struct rcu_head *rhp)
 {
 	struct task_struct *tsk = container_of(rhp, struct task_struct, rcu);
 
+	kprobe_flush_task(tsk);
 	perf_event_delayed_put(tsk);
 	trace_sched_process_free(tsk);
 	put_task_struct(tsk);
