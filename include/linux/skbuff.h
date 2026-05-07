11941c48c8dfb1e50bbbe1c8b441d9a7a001572f
Alexander Winkowski <dereference23@outlook.com>
2026-05-07 21:22:34 +0000

    Revert "ANDROID: fix up struct sk_buf ABI breakage"
    This reverts commit 5bb30ff63f0a934ea1cb9b39a07d9052bec617cf.
    
    Change-Id: I967b3b5df53d9996426e97ec3fdbaeb5f0474518
    Signed-off-by: Alexander Winkowski <dereference23@outlook.com>

--- include/linux/skbuff.h
+++ include/linux/skbuff.h
@@ -837,6 +837,7 @@ struct sk_buff {
 #ifdef CONFIG_TLS_DEVICE
 	__u8			decrypted:1;
 #endif
+	__u8			scm_io_uring:1;
 
 #ifdef CONFIG_NET_SCHED
 	__u16			tc_index;	/* traffic control index */
@@ -885,21 +886,7 @@ struct sk_buff {
 
 	); /* end headers group */
 
-	/* Android KABI preservation.
-	 *
-	 * "open coded" version of ANDROID_KABI_USE() to pack more
-	 * fields/variables into the space that we have.
-	 *
-	 * scm_io_uring is from 04df9719df18 ("io_uring/af_unix: defer
-	 * registered files gc to io_uring release")
-	 */
-	_ANDROID_KABI_REPLACE(_ANDROID_KABI_RESERVE(1),
-			 struct {
-				__u8 scm_io_uring:1;
-				__u8 android_kabi_reserved1_padding1;
-				__u16 android_kabi_reserved1_padding2;
-				__u32 android_kabi_reserved1_padding3;
-				});
+	ANDROID_KABI_RESERVE(1);
 	ANDROID_KABI_RESERVE(2);
 
 	/* These elements must be at the end, see alloc_skb() for details.  */
