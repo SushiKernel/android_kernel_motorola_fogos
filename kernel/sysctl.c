ca7efa8a626dd67a02994a874b1e5291702a9e87
MondayNitro <87489540+MondayNitro@users.noreply.github.com>
2026-05-15 14:18:26 +0000

    Revert "sched: promote nodes out of CONFIG_SCHED_DEBUG"
    This reverts commit 14dafffa003b13281f665daa85ce1e10c0238282.

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -332,12 +332,14 @@ static struct ctl_table sysctl_base_table[] = {
 	{ }
 };
 
+#ifdef CONFIG_SCHED_DEBUG
 static int min_sched_base_slice = 100000;		/* 100 usecs */
 static int max_sched_base_slice = NSEC_PER_SEC;	/* 1 second */
 #ifdef CONFIG_SMP
 static int min_sched_tunable_scaling = SCHED_TUNABLESCALING_NONE;
 static int max_sched_tunable_scaling = SCHED_TUNABLESCALING_END-1;
 #endif /* CONFIG_SMP */
+#endif /* CONFIG_SCHED_DEBUG */
 
 #ifdef CONFIG_COMPACTION
 static int min_extfrag_threshold;
@@ -378,6 +380,7 @@ static int bpf_stats_handler(struct ctl_table *table, int write,
 #endif
 
 static struct ctl_table kern_table[] = {
+#ifdef CONFIG_SCHED_DEBUG
 	{
 		.procname	= "sysctl_sched_base_slice",
 		.data		= &sysctl_sched_base_slice,
@@ -413,7 +416,6 @@ static struct ctl_table kern_table[] = {
 		.mode		= 0644,
 		.proc_handler	= proc_dointvec,
 	},
-#ifdef CONFIG_SCHED_DEBUG
 	{
 		.procname	= "sched_nr_migrate",
 		.data		= &sysctl_sched_nr_migrate,
