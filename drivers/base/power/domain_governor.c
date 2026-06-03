df154f2afd4e5a385faf397bc5a98f2bfa477f2e
MondayNitro <87489540+MondayNitro@users.noreply.github.com>
2026-06-03 14:19:07 +0000

    treewide: adjust for upstream PM QoS API changes

--- drivers/base/power/domain_governor.c
+++ drivers/base/power/domain_governor.c
@@ -267,7 +267,7 @@ static bool cpu_power_down_ok(struct dev_pm_domain *pd)
 	if (!(genpd->flags & GENPD_FLAG_CPU_DOMAIN))
 		return true;
 
-	global_constraint = pm_qos_request(PM_QOS_CPU_DMA_LATENCY);
+	global_constraint = S64_MAX;
 	/*
 	 * Find the next wakeup for any of the online CPUs within the PM domain
 	 * and its subdomains. Note, we only need the genpd->cpus, as it already
