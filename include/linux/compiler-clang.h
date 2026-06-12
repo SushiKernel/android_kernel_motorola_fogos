ff91aba979a4e19c3a3febd8a3db60dea1cd83b4
Alexander Winkowski <dereference23@outlook.com>
2026-06-12 12:44:37 +0000

    treewide: Update CFI from upstream
    Change-Id: I35602b7d0ce8dc5dea83686b2d6fae157c10f765
    Signed-off-by: Alexander Winkowski <dereference23@outlook.com>

--- include/linux/compiler-clang.h
+++ include/linux/compiler-clang.h
@@ -46,10 +46,9 @@
 #define __norecordmcount \
 	__attribute__((__section__(".text..ftrace")))
 #endif
-
+#endif
 
 #define __nocfi		__attribute__((__no_sanitize__("cfi")))
-#endif
 
 #define __diag_clang_all(s)	__diag(s)
 
