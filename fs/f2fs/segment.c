615e5e74ed3acc52a57a1e091e469bd2ea4bc620
Chao Yu <chao@kernel.org>
2026-07-09 15:53:40 +0000

    f2fs: fix to avoid NULL pointer dereference in f2fs_issue_flush()
    With below two cases, it will cause NULL pointer dereference when
    accessing SM_I(sbi)->fcc_info in f2fs_issue_flush().
    
    a) If kthread_run() fails in f2fs_create_flush_cmd_control(), it will
    release SM_I(sbi)->fcc_info,
    
    - mount -o noflush_merge /dev/vda /mnt/f2fs
    - mount -o remount,flush_merge /dev/vda /mnt/f2fs  -- kthread_run() fails
    - dd if=/dev/zero of=/mnt/f2fs/file bs=4k count=1 conv=fsync
    
    b) we will never allocate memory for SM_I(sbi)->fcc_info w/ below
    testcase,
    
    - mount -o ro /dev/vda /mnt/f2fs
    - mount -o rw,remount /dev/vda /mnt/f2fs
    - dd if=/dev/zero of=/mnt/f2fs/file bs=4k count=1 conv=fsync
    
    In order to fix this issue, let change as below:
    - fix error path handling in f2fs_create_flush_cmd_control().
    - allocate SM_I(sbi)->fcc_info even if readonly is on.
    
    Signed-off-by: Chao Yu <chao@kernel.org>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>

--- fs/f2fs/segment.c
+++ fs/f2fs/segment.c
@@ -732,8 +732,7 @@ init_thread:
 				"f2fs_flush-%u:%u", MAJOR(dev), MINOR(dev));
 	if (IS_ERR(fcc->f2fs_issue_flush)) {
 		err = PTR_ERR(fcc->f2fs_issue_flush);
-		kfree(fcc);
-		SM_I(sbi)->fcc_info = NULL;
+		fcc->f2fs_issue_flush = NULL;
 		return err;
 	}
 
@@ -4762,11 +4761,9 @@ int f2fs_build_segment_manager(struct f2fs_sb_info *sbi)
 
 	init_rwsem(&sm_info->curseg_lock);
 
-	if (!f2fs_readonly(sbi->sb)) {
-		err = f2fs_create_flush_cmd_control(sbi);
-		if (err)
-			return err;
-	}
+	err = f2fs_create_flush_cmd_control(sbi);
+	if (err)
+		return err;
 
 	err = create_discard_cmd_control(sbi);
 	if (err)
