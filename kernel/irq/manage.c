5e1bbb3e6f57da271e581661c2c0bc1604d20a47
Tanner Love <tannerlove@google.com>
2026-08-13 05:47:54 +0000

    genirq: Change force_irqthreads to a static key
    With CONFIG_IRQ_FORCED_THREADING=y, testing the boolean force_irqthreads
    could incur a cache line miss in invoke_softirq() and other places.
    
    Replace the test with a static key to avoid the potential cache miss.
    
    [ tglx: Dropped the IDE part, removed the export and updated blk-mq ]
    
    Suggested-by: Eric Dumazet <edumazet@google.com>
    Signed-off-by: Tanner Love <tannerlove@google.com>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Reviewed-by: Eric Dumazet <edumazet@google.com>
    Reviewed-by: Kees Cook <keescook@chromium.org>
    Link: https://lore.kernel.org/r/20210602180338.3324213-1-tannerlove.kernel@gmail.com

--- kernel/irq/manage.c
+++ kernel/irq/manage.c
@@ -24,12 +24,11 @@
 #include "internals.h"
 
 #if defined(CONFIG_IRQ_FORCED_THREADING) && !defined(CONFIG_PREEMPT_RT)
-__read_mostly bool force_irqthreads;
-EXPORT_SYMBOL_GPL(force_irqthreads);
+DEFINE_STATIC_KEY_FALSE(force_irqthreads_key);
 
 static int __init setup_forced_irqthreads(char *arg)
 {
-	force_irqthreads = true;
+	static_branch_enable(&force_irqthreads_key);
 	return 0;
 }
 early_param("threadirqs", setup_forced_irqthreads);
@@ -1249,8 +1248,8 @@ static int irq_thread(void *data)
 
 	irq_thread_set_ready(desc, action);
 
-	if (force_irqthreads && test_bit(IRQTF_FORCED_THREAD,
-					&action->thread_flags))
+	if (force_irqthreads() && test_bit(IRQTF_FORCED_THREAD,
+					   &action->thread_flags))
 		handler_fn = irq_forced_thread_fn;
 	else
 		handler_fn = irq_thread_fn;
@@ -1311,7 +1310,7 @@ EXPORT_SYMBOL_GPL(irq_wake_thread);
 
 static int irq_setup_forced_threading(struct irqaction *new)
 {
-	if (!force_irqthreads)
+	if (!force_irqthreads())
 		return 0;
 	if (new->flags & (IRQF_NO_THREAD | IRQF_PERCPU | IRQF_ONESHOT))
 		return 0;
