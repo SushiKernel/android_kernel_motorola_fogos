390e288285f22b15dfc8d2cff30ad84917826bc1
Yury Norov <yury.norov@gmail.com>
2026-08-25 10:56:03 +0000

    lib: add fast path for find_first_*_bit() and find_last_bit()
    Similarly to bitmap functions, users would benefit if we'll handle a case
    of small-size bitmaps that fit into a single word.
    
    While here, move the find_last_bit() declaration to bitops/find.h where
    other find_*_bit() functions sit.
    
    Link: https://lkml.kernel.org/r/20210401003153.97325-11-yury.norov@gmail.com
    Signed-off-by: Yury Norov <yury.norov@gmail.com>
    Acked-by: Rasmus Villemoes <linux@rasmusvillemoes.dk>
    Acked-by: Andy Shevchenko <andy.shevchenko@gmail.com>
    Cc: Alexey Klimov <aklimov@redhat.com>
    Cc: Arnd Bergmann <arnd@arndb.de>
    Cc: David Sterba <dsterba@suse.com>
    Cc: Dennis Zhou <dennis@kernel.org>
    Cc: Geert Uytterhoeven <geert@linux-m68k.org>
    Cc: Jianpeng Ma <jianpeng.ma@intel.com>
    Cc: Joe Perches <joe@perches.com>
    Cc: John Paul Adrian Glaubitz <glaubitz@physik.fu-berlin.de>
    Cc: Josh Poimboeuf <jpoimboe@redhat.com>
    Cc: Rich Felker <dalias@libc.org>
    Cc: Stefano Brivio <sbrivio@redhat.com>
    Cc: Wei Yang <richard.weiyang@linux.alibaba.com>
    Cc: Wolfram Sang <wsa+renesas@sang-engineering.com>
    Cc: Yoshinori Sato <ysato@users.osdn.me>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- include/asm-generic/bitops/find.h
+++ include/asm-generic/bitops/find.h
@@ -5,6 +5,9 @@
 extern unsigned long _find_next_bit(const unsigned long *addr1,
 		const unsigned long *addr2, unsigned long nbits,
 		unsigned long start, unsigned long invert, unsigned long le);
+extern unsigned long _find_first_bit(const unsigned long *addr, unsigned long size);
+extern unsigned long _find_first_zero_bit(const unsigned long *addr, unsigned long size);
+extern unsigned long _find_last_bit(const unsigned long *addr, unsigned long size);
 
 #ifndef find_next_bit
 /**
@@ -107,8 +110,17 @@ extern unsigned long find_first_and_and_bit(const unsigned long *addr1,
  * Returns the bit number of the first set bit.
  * If no bits are set, returns @size.
  */
-extern unsigned long find_first_bit(const unsigned long *addr,
-				    unsigned long size);
+static inline
+unsigned long find_first_bit(const unsigned long *addr, unsigned long size)
+{
+	if (small_const_nbits(size)) {
+		unsigned long val = *addr & GENMASK(size - 1, 0);
+
+		return val ? __ffs(val) : size;
+	}
+
+	return _find_first_bit(addr, size);
+}
 
 /**
  * find_first_zero_bit - find the first cleared bit in a memory region
@@ -118,8 +130,17 @@ extern unsigned long find_first_bit(const unsigned long *addr,
  * Returns the bit number of the first cleared bit.
  * If no bits are zero, returns @size.
  */
-extern unsigned long find_first_zero_bit(const unsigned long *addr,
-					 unsigned long size);
+static inline
+unsigned long find_first_zero_bit(const unsigned long *addr, unsigned long size)
+{
+	if (small_const_nbits(size)) {
+		unsigned long val = *addr | ~GENMASK(size - 1, 0);
+
+		return val == ~0UL ? size : ffz(val);
+	}
+
+	return _find_first_zero_bit(addr, size);
+}
 #else /* CONFIG_GENERIC_FIND_FIRST_BIT */
 
 #ifndef find_first_bit
@@ -130,5 +151,26 @@ extern unsigned long find_first_zero_bit(const unsigned long *addr,
 #endif
 
 #endif /* CONFIG_GENERIC_FIND_FIRST_BIT */
+
+#ifndef find_last_bit
+/**
+ * find_last_bit - find the last set bit in a memory region
+ * @addr: The address to start the search at
+ * @size: The number of bits to search
+ *
+ * Returns the bit number of the last set bit, or size.
+ */
+static inline
+unsigned long find_last_bit(const unsigned long *addr, unsigned long size)
+{
+	if (small_const_nbits(size)) {
+		unsigned long val = *addr & GENMASK(size - 1, 0);
+
+		return val ? __fls(val) : size;
+	}
+
+	return _find_last_bit(addr, size);
+}
+#endif
 
 #endif /*_ASM_GENERIC_BITOPS_FIND_H_ */
